c191bbade053e537a1b45d1c02dc7c62a01165d0
liugang <924874135@163.com>
2019-08-06 15:12:41 +0800

    添加多线程示例

--- app.js
+++ app.js
@@ -1,3 +1,20 @@
+//app.js
+const utils = require('./utils/util.js')
+const worker = wx.createWorker('workers/request/index.js')
+
+worker.postMessage({
+	msg: 'hello from AppService',
+	buffer: utils.str2ab('hello arrayBuffer from AppService')
+})
+
+worker.onMessage(function (msg) {
+	console.log('[AppService] onWorkerMessage', msg)
+	const buffer = msg.buffer
+	console.log('[AppService] on worker buffer', utils.ab2str(buffer))
+})
+
+console.log(`[AppService] on worker create: ${worker.onMessage}`)
+
 App({
     onLaunch: function (options) {
         console.log("===========onLaunch===========");
